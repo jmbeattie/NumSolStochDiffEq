b164dc819fa23c1072216bae89a8d443e20bf48f
jbeattie <jbeattie@reyesholdings.com>
2022-07-02 01:49:52 -0500

    Code now computes uniform distribution between 0 and 1 and can be scaled.

--- rng.cpp
+++ rng.cpp
@@ -1,17 +1,24 @@
 #include <iostream>
 #include <math.h>
 #include <list>
+#include <string>
+
 
 using namespace std;
 
-list<uint32_t> randUnif(int seed, int b, int n, int a=65539, uint32_t c=pow(2,31));
+
+const uint32_t RANDOM_SEED = 424242;
 
 
-int main()
+list<uint32_t> randUnif(int n=1, int b=0, int a=65539, uint32_t c=pow(2,31));
+list<double> rdn(int n=1, int lo=0, int hi=1);
+
+
+int main(int argc, char* argv[])
 {
-    list<uint32_t> randNums = randUnif(5, 0, 10);
+    list<double> randNums = rdn(stoi(argv[1]), -100, 0);
 
-    for(list<uint32_t>::iterator it = randNums.begin(); !(it == randNums.end()); it++)
+    for(list<double>::iterator it = randNums.begin(); !(it == randNums.end()); it++)
     {
         cout << *it << "\n";
     }
@@ -20,14 +27,28 @@ int main()
 }
 
 
-list<uint32_t> randUnif(int seed, int b, int n, int a, uint32_t c)
+list<double> rdn(int n, int lo, int hi)
 {
-    int x0 = seed;
+    list<uint32_t> randNums = randUnif(n);
+    list<double> randFloats;
+    int magnitude = hi - lo;
+    int size = randNums.size(); // must assign first then reference to avoid short circuit
+    for(list<uint32_t>::iterator it = randNums.begin(); !(it == randNums.end()); it++)
+    {
+        randFloats.push_back((*it/pow(2,31)) * magnitude + lo);
+    }
+    return randFloats;
+}
+
+
+list<uint32_t> randUnif(int n, int b, int a, uint32_t c)
+{
+    int x0 = RANDOM_SEED;
     list<uint32_t> x;
     for(int i = 0; i < n; i++)
     {
-        x.push_front((a*x0 + b) % c);
-        x0 = x.front();
+        x.push_back((a*x0 + b) % c);
+        x0 = x.back();
     }
     return x;
 }
